551f64e2d4116abb501c7f343efe7ecd04537566
Tony Heupel <tonyheupel@gmail.com>
2012-06-12 13:39:04 -0700

    use deleteAttachment

--- ravenapp.js
+++ ravenapp.js
@@ -1,7 +1,11 @@
 var fs = require('fs')
   , ravendb = require('ravendb')
+  , ArgumentParser = require('argparse').ArgumentParser
 
-var addFiles = function(appName, rootDir, db, callback) {
+
+var version = '0.0.1' // update package.json
+
+var addFiles = function(appName, appDir, rootDir, db, callback) {
   fs.readdir(rootDir, function(err, files) {
     files.forEach(function(filename) {
       filename = rootDir + '/' + filename
@@ -9,7 +13,7 @@ var addFiles = function(appName, rootDir, db, callback) {
      fs.stat(filename, function (err, stat) {
         if (err) { callback(err); return; }
         if (stat.isDirectory()) {
-          addFiles(appName, filename, db, function(err, cb) {
+          addFiles(appName, appDir, filename, db, function(err, cb) {
             if (err) { callback(err); return;}
             else console.log('Added files for "' + filename + '"')
           })
@@ -35,7 +39,7 @@ var addFiles = function(appName, rootDir, db, callback) {
           docId += filename.replace(appDir, '')
 
           // Add deleteAttachment to ravendb module
-          db.apiDeleteCall(db.getAttachmentUrl(docId), function(err, resp) {
+          db.deleteAttachment(docId, function(err, resp) {
             db.saveAttachment(docId, fs.readFileSync(filename, 'UTF-8'), { 'Content-Type': mimeType }, function(err, result) {
               if (err) { callback(err); return; }
               else console.log('Saved "' + filename + '" to "' + docId + '"')
@@ -48,15 +52,51 @@ var addFiles = function(appName, rootDir, db, callback) {
   })
 }
 
-var saveApp = function(appName, appDir, dbUrl, cb) {
-  addFiles(appName, appDir, ravendb(dbUrl), function(e,r) {
+var saveApp = function(appName, appDir, dbUrl, dbName, cb) {
+  if (typeof dbName === 'function') dbName = null
+
+  addFiles(appName, appDir, appDir, ravendb(dbUrl, dbName), function(e,r) {
     if (e) console.log('Error in saveApp: ' + e)
     else console.log('Finished saving app: ' + r)
   })
 }
 
-var appName = 'studio'
-  , dbUrl = 'http://localhost/ravendb'
-  , appDir = '../http-ravendb/public'
+var parser = new ArgumentParser({
+  'version': version,
+  addHelp: true,
+  description: 'RavenApp builder'
+});
+parser.addArgument(
+  [ '-d', '--directory' ],
+  {
+    help: 'base directory for the RavenApp',
+    dest: 'directory'
+  }
+)
+parser.addArgument(
+  [ '-n', '--name' ],
+  {
+    help: 'the name of the RavenApp',
+    dest: 'name'
+  }
+)
+parser.addArgument(
+  [ '-s', '--store' ],
+  {
+    help: 'specify which data store to use (defaults to http://localhost:8080 if not specfied)',
+    defaultValue: 'http://localhost:8080',
+    dest: 'store'
+  }
+)
+parser.addArgument(
+  [ '-db', '--database'],
+  {
+    help: 'specify which database to use (defaults to "Default" if not specified)',
+    defaultValue: 'Default',
+    dest: 'database'
+  }
+)
 
-saveApp(appName, appDir, dbUrl)
+var args = parser.parseArgs();
+
+saveApp(args.name, args.directory, args.store, args.database)
