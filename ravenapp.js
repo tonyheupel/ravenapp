9ee442dc673c57c717bd54c2235dc45bdcf0b93a
Tony Heupel <tonyheupel@gmail.com>
2012-06-17 08:20:16 -0700

    fix non-ravenhq case

--- ravenapp.js
+++ ravenapp.js
@@ -4,7 +4,7 @@ var fs = require('fs')
   , ArgumentParser = require('argparse').ArgumentParser
 
 
-var version = '0.0.1' // update package.json
+var version = '0.0.2' // update package.json
 
 var addFiles = function(appName, appDir, rootDir, db, callback) {
   fs.readdir(rootDir, function(err, files) {
@@ -58,6 +58,11 @@ var saveApp = function(args, cb) {
         if (e) console.log('Error in saveApp: ' + e)
         else console.log('Finished saving app: ' + r)
       })
+    })
+  } else {
+    addFiles(appName, appDir, appDir, db, function(e,r) {
+      if (e) console.log('Error in saveApp: ' + e)
+      else console.log('Finished saving app: ' + r)
     })
   }  
 }
