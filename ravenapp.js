239a03391a93ff4553aa980c72ba7c51deb32122
Tony Heupel <tonyheupel@gmail.com>
2012-07-21 11:38:12 -0700

    Convert to coffeescript

--- ravenapp.js
+++ ravenapp.js
@@ -1,81 +1,79 @@
-var fs = require('fs')
-  , path = require('path')
-  , ravendb = require('ravendb')
-  , ArgumentParser = require('argparse').ArgumentParser
+fs = require('fs')
+path = require('path')
+ravendb = require('ravendb')
+ArgumentParser = require('argparse').ArgumentParser
 
 
-var version = '0.0.2' // update package.json
+version = '0.0.3' # update package.json
 
-var addFiles = function(appName, appDir, rootDir, db, callback) {
-  fs.readdir(rootDir, function(err, files) {
-    files.forEach(function(filename) {
-      filename = rootDir + '/' + filename
+addFiles = (appName, appDir, rootDir, db, callback) ->
+  fs.readdir rootDir, (err, files) ->
+    files.forEach (filename) ->
+      filename = "#{rootDir}/#{filename}"
 
-     fs.stat(filename, function (err, stat) {
-        if (err) { callback(err); return; }
-        if (stat.isDirectory()) {
-          addFiles(appName, appDir, filename, db, function(err, cb) {
-            if (err) { callback(err); return;}
-            else console.log('Added files for "' + filename + '"')
-          })
-        } else {
-          // Save Attachment here
-          var appRoot = 'apps/' + appName
-          var docId = appRoot.replace(appDir, '')
+      fs.stat filename, (err, stat) ->
+        if err? 
+          callback(err)
+          return
 
-          //if (!/index\.html$/.test(filename))
+        if stat.isDirectory()
+          addFiles appName, appDir, filename, db, (err, cb) ->
+            if err?
+              callback(err)
+              return
+            else
+              console.log "Added files for #{filename}"
+
+        else
+          # Save Attachment here
+          appRoot = "apps/#{appName}"
+          docId = appRoot.replace(appDir, '')
+
+          # if (!/index\.html$/.test(filename))
           docId += filename.replace(appDir, '')
 
-          // Add deleteAttachment to ravendb module
-          db.deleteAttachment(docId, function(err, resp) {
-            db.saveAttachment(docId, fs.createReadStream(filename), function(err, result) {
-              if (err) { callback(err); return; }
-              else console.log('Saved "' + filename + '" to "' + docId + '"')
-            })
-          })
+          # Add deleteAttachment to ravendb module
+          db.deleteAttachment docId, (err, resp) ->
+            db.saveAttachment docId, fs.createReadStream(filename), (err, result) ->
+              if err?
+                callback(err)
+                return
+              else
+                console.log "Saved \"#{filename}\" to \"#{docId}\""
 
-        }
-      })
-    })
-  })
-}
 
-var saveApp = function(args, cb) {
 
-  // Need to strip trailing /
-  var appDir = args.directory
-    , appName = args.name
-    , dbUrl = args.store
-    , dbName = args.database
-    , ravenHqApiKey = args.apiKey
+saveApp = (args, cb) ->
+  # Need to strip trailing /
+  appDir = args.directory
+  appName = args.name
+  dbUrl = args.store
+  dbName = args.database
+  ravenHqApiKey = args.apiKey
 
-  appDir = appDir[appDir.length - 1] === '/' ? appDir.substring(0, appDir.length - 1) : appDir
+  appDir = if appDir[appDir.length - 1] is '/' then appDir.substring(0, appDir.length - 1) else appDir
 
-  var db = ravendb(dbUrl, dbName)
-  if (/ravenhq\.com/.test(dbUrl) && ravenHqApiKey) {
-    db.useRavenHq(ravenHqApiKey, function(err, auth) { // If api key is null, this has no effect
-      addFiles(appName, appDir, appDir, db, function(e,r) {
-        if (e) console.log('Error in saveApp: ' + e)
-        else console.log('Finished saving app: ' + r)
+  db = ravendb(dbUrl, dbName)
+  if /ravenhq\.com/.test(dbUrl) and ravenHqApiKey
+    db.useRavenHq ravenHqApiKey, (err, auth) -> # If api key is null, this has no effect
+      addFiles appName, appDir, appDir, db, (e,r) ->
+        if e? then console.log "Error in saveApp: #{e}"
+        else console.log "Finished saving app: #{r}"
 
         cb(e,r)
-      })
-    })
-  } else {
-    addFiles(appName, appDir, appDir, db, function(e,r) {
-      if (e) console.log('Error in saveApp: ' + e)
-      else console.log('Finished saving app: ' + r)
+  else
+    addFiles appName, appDir, appDir, db, (e,r) ->
+      if e? console.log "Error in saveApp: #{e}"
+      else console.log "Finished saving app: #{r}"
 
       cb(e,r)
-    })
-  }
-}
 
-var parser = new ArgumentParser({
+
+parser = new ArgumentParser({
   'version': version,
   addHelp: true,
   description: 'RavenApp builder'
-});
+})
 parser.addArgument(
   [ '-d', '--directory' ],
   {
@@ -115,9 +113,8 @@ parser.addArgument(
   }
 )
 
-var args = parser.parseArgs();
+args = parser.parseArgs()
 
-saveApp(args, function(err, resp) {
- if (err) console.log(err)
- else console.error('Done.')
-})
+saveApp args, (err, resp) ->
+ if err? console.log err
+ else console.log 'Done.'
